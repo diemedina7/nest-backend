13874826a826bb58a20ecbf47933c164a3ad4f7c
Diego Medina <diegocab456@gmail.com>
2023-10-11 23:19:17 -0300

    login endpoint - verificacion de datos

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -7,6 +7,7 @@ import * as bcryptjs from 'bcryptjs'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -40,6 +41,26 @@ export class AuthService {
       }
       throw new InternalServerErrorException('Error fatal!!');
     }
+  }
+
+  async login( loginDto: LoginDto ) {
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email: email })
+
+    if (!user) {
+      throw new UnauthorizedException('Not valid credential - email');
+    }
+
+    if (!bcryptjs.compareSync( password, user.password )) {
+      throw new UnauthorizedException('Not valid credential - password');
+    }
+
+    const { password:_, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: 'ABC'
+    };
   }
 
   findAll() {
